dae3835833db98f3ced72c5e19245832a69603d2
Thomas Newton <thomas.w.newton@gmail.com>
2020-12-03 09:48:36 +0000

    Add logmarks to helloworld example

--- benchmarks/source/riscv/HelloWorld/hello.c
+++ benchmarks/source/riscv/HelloWorld/hello.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <printf.h>
+#include "../../superh/port/logmarkers.h"
 
 
-int
-main(void)
+int main(void)
 {
+	LOGMARK(0);
 	printf_("Hello, World.\n");
+	LOGMARK(1);
 
 	return 0;
 }
