ac8c6974099cd4a312d75609413fb3cbecbcfeca
Thomas Newton <thomas.w.newton@gmail.com>
2020-12-08 14:28:24 +0000

    Debugging

--- benchmarks/source/riscv/bme680/hum/original/original.c
+++ benchmarks/source/riscv/bme680/hum/original/original.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <printf.h>
 #include <stdint.h>
+#include <math.h>
 #include "../../../../superh/port/logmarkers.h"
 #include "../../BME680_driver/bme680.c"
 #include "../../BME680_driver/bme680.h"
@@ -12,16 +13,37 @@ int main(void)
 	dev.calib.par_t1 = 26372;
 	dev.calib.par_t2 = 26190;
 	dev.calib.par_t3 = 3;
-	// TODO: Add the humidity calibration constants 
+	
+	dev.calib.par_h1 = 785; 
+	dev.calib.par_h2 = 1009;
+	dev.calib.par_h3 = 0;
+	dev.calib.par_h4 = 45;
+	dev.calib.par_h5 = 20;
+	dev.calib.par_h6 = 120;
+	dev.calib.par_h7 = 156;
 
-	uint32_t temp_adc = 543639;
-	uint32_t hum_adc = 643639;
+	uint32_t temp_adc = 469993;
+	uint32_t hum_adc = 21975;
 
 	LOGMARK(0);
-	calc_temperature(temp_adc, &dev);
-	float hum = calc_humure(hum_adc, &dev);
+	float temp = calc_temperature(temp_adc, &dev);
+	float hum = calc_humidity(hum_adc, &dev);
+	// 469993.0     21975.0              15.000098             50.002233
 	LOGMARK(1);
 
+	if (roundf(temp) == 15) {
+		printf_("temp_correct\n");
+	} else {
+		printf_("temp_wrong\n");
+	}
+
+	if (roundf(hum) == 50) {
+		printf_("hum_correct\n");
+	} else {
+		printf_("hum_wrong\n");
+	}
+
+	printf_("Converted temp = %f\n", temp);
 	printf_("Converted hum = %f\n", hum);
 
 	return 0;
--- benchmarks/source/riscv/bme680/press/original/original.c
+++ benchmarks/source/riscv/bme680/press/original/original.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <printf.h>
 #include <stdint.h>
+#include <math.h>
 #include "../../../../superh/port/logmarkers.h"
 #include "../../BME680_driver/bme680.c"
 #include "../../BME680_driver/bme680.h"
@@ -13,7 +14,7 @@ int main(void)
 	dev.calib.par_t2 = 26190;
 	dev.calib.par_t3 = 3;
 	
-	dev.calib.par_p1 =36338; 
+	dev.calib.par_p1 = 36338; 
 	dev.calib.par_p2 = 55104;
 	dev.calib.par_p3 = 88;
 	dev.calib.par_p4 = 7194;
@@ -24,14 +25,27 @@ int main(void)
 	dev.calib.par_p9 = 62990;
 	dev.calib.par_p10 = 30;
 
-	uint32_t temp_adc = 492409;
-	uint32_t press_adc = 672774;
+	uint32_t temp_adc = 527136;
+	uint32_t press_adc = 357152;
 
 	LOGMARK(0);
 	float temp = calc_temperature(temp_adc, &dev);
 	float press = calc_pressure(press_adc, &dev);
 	// temp=22.000028 press=69999.935282
+	// 21.000093            49999.992044
 	LOGMARK(1);
+
+	if (roundf(temp) == 21) {
+		printf_("temp_correct\n");
+	} else {
+		printf_("temp_wrong\n");
+	}
+
+	if (roundf(press) == 26036) {
+		printf_("press_correct\n");
+	} else {
+		printf_("press_wrong\n");
+	}
 
 	printf_("Converted temp = %f\n", temp);
 	printf_("Converted press = %f\n", press);
