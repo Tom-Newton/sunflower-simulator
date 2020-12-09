93ebbb9b96b837134178eb8b28e498ee8d0eb13c
Thomas Newton <thomas.w.newton@gmail.com>
2020-12-09 14:13:36 +0000

    Test complexity of a single exponential

---
+++ benchmarks/source/riscv/small_tests/exponenitial/exponential.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <printf.h>
+#include <stdint.h>
+#include <math.h>
+#include "../../../superh/port/logmarkers.h"
+
+int main(void)
+{
+	float x = 12.2334294;
+
+	LOGMARK(0);
+	expf(x);
+	LOGMARK(1);
+	
+	return 0;
+}
---
+++ benchmarks/source/riscv/small_tests/exponenitial/run.m
@@ -0,0 +1,5 @@
+newnode riscv
+sizemem		900000000
+srecl		exponential.sr
+run
+on
