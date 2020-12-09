26ea307135b58f87a3b904e979d7b5009da58af7
Thomas Newton <thomas.w.newton@gmail.com>
2020-12-09 12:40:16 +0000

    Working Gaussian process conversion

--- benchmarks/source/riscv/bme680/press/gaussian_process/gaussian_process.c
+++ benchmarks/source/riscv/bme680/press/gaussian_process/gaussian_process.c
@@ -2,14 +2,14 @@
 #include <printf.h>
 #include <stdint.h>
 #include <math.h>
-// #include "../../../../superh/port/logmarkers.h"
+#include "../../../../superh/port/logmarkers.h"
 
 float gaussian_kernel(float x[2], float y[2])
 {
 	return 172.55812E9 * expf(-((x[0] - y[0]) * (x[0] - y[0]) + (x[1] - y[1]) * (x[1] - y[1])) / 11.25731E12);
 }
 
-float (*evaluate_cross_kernel(float x_star[2]))[9]
+void evaluate_cross_kernel(float x_star[2], float (*cross_kernel)[9])
 {
 	static float train_input_points[9][2] = {
 		{738541.0, 293804.0},
@@ -22,30 +22,45 @@ float (*evaluate_cross_kernel(float x_star[2]))[9]
 		{290846.0, 492409.0},
 		{350295.0, 694076.0}};
 
-	float evaluated_cross_kernel[9];
-
 	for (int i = 0; i < 9; i++)
 	{
-		evaluated_cross_kernel[i] = gaussian_kernel(x_star, train_input_points[i]);
+		(*cross_kernel)[i] = gaussian_kernel(x_star, train_input_points[i]);
 	}
-	return &evaluated_cross_kernel;
 }
 
-// [0.71952032 - 0.84608666 0.77310838 - 1.74601382 1.98624047 - 1.72270744 1.02691494 - 1.14059084 0.94990313]
+float mean(float x_star[2])
+{
+	static float parameter_mean[9] = {
+		0.00096469,
+		-0.00290255,
+		0.00210706,
+		-0.00382518,
+		0.01025932,
+		-0.00685208,
+		0.00292186,
+		-0.00748054,
+		0.0048086};
 
-// bme680_press bme680_temp
+	float cross_kernel[9];
+	evaluate_cross_kernel(x_star, &cross_kernel);
+
+	float mean = 0;
+	for (int i = 0; i < 9; i++)
+	{
+		mean += cross_kernel[i] * parameter_mean[i];
+	}
+	return mean;
+}
 
 int main(void)
 {
-	uint32_t temp_adc = 543639;
-	uint32_t press_adc = 643639;
+	float x_star[2] = {738541, 293804};
 
-	// evaluate_test_kernel({})
+	LOGMARK(0);
+	float press = mean(x_star);
+	LOGMARK(1);
 
-	// LOGMARK(0);
-	// LOGMARK(1);
-
-	// printf_("Converted press = %f\n", press);
+	printf_("Converted press = %f\n", press);
 
 	return 0;
 }
